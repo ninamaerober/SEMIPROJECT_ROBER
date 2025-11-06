bd02dc28f0e49af23d4eec373836051b8f93a236
ninamaerober <ninamaerober.202300768@gmail.com>
2025-11-06 10:42:30 +0800

    Update App.jsx

--- student-ai-app-rober/src/App.jsx
+++ student-ai-app-rober/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from  "react-router";
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import StudentsPage from "./pages/StudentsPage";
 import SubjectsPage from "./pages/SubjectsPage";
 import GradesPage from "./pages/GradesPage";
@@ -10,10 +12,12 @@ export default function App() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/students" element={<StudentsPage />} />
         <Route path="/subjects" element={<SubjectsPage />} />
-        <Route path="/grades" element={<GradesPage />} />
+        <Route path="/grades" element={<GradesPage />} /> 
       </Routes>
       <Toaster />
     </BrowserRouter>
